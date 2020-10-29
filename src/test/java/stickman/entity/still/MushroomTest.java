cf5e1d9d522be93aaa9336fff6b7e2cd3c7611cb
lmac4334 <lmac4334@uni.sydney.edu.au>
2020-10-29 12:46:22 +1100

    Mushroom testing

--- src/test/java/stickman/entity/still/MushroomTest.java
+++ src/test/java/stickman/entity/still/MushroomTest.java
@@ -1,6 +1,10 @@
 package stickman.entity.still;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import stickman.entity.moving.player.StickMan;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Mushroom object that the player can pick up to get the ability to shoot.
@@ -12,6 +16,37 @@ public class MushroomTest {
     public void setup() {
         this.m = new Mushroom(0,0);
     }
+
+    @Test
+    public void testConstructor() {
+        Mushroom m = new Mushroom(0, 0);
+        assertNotNull(m);
+    }
+
+    @Test
+    public void testCoords() {
+        assertEquals(0, m.getXPos());
+        assertEquals(0, m.getYPos());
+    }
+
+    @Test
+    public void testSolidity() {
+        assertFalse(m.isSolid());
+    }
+
+    @Test
+    public void testActive() {
+        assertTrue(m.isActive());
+    }
+
+    @Test
+    public void testInteractionMushroom() {
+        StickMan h = new StickMan(0, 0, "large", null);
+        Mushroom interact = new Mushroom(0,0);
+        interact.interact(h);
+        assertFalse(interact.isActive());
+    }
+
 
 
 }
